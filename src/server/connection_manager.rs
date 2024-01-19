8c2e305fa4b79ce95c8dd32a8a337dfda25ce80d
JadKHaddad <jadkhaddad@gmail.com>
2024-01-19 16:15:21 +0100

    ws stuff

--- src/server/connection_manager.rs
+++ src/server/connection_manager.rs
@@ -1,8 +1,10 @@
-use std::net::SocketAddr;
-
 use super::ws::{ClientMessage, ServerMessage};
 use axum::extract::ws::{Message, WebSocket};
-use futures::{SinkExt, StreamExt};
+use futures::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
+use std::net::SocketAddr;
 use tokio::sync::{broadcast, mpsc};
 
 #[derive(Clone)]
@@ -21,6 +23,79 @@ impl ConnectionManager {
         let _ = self.broadcast_sender.send(msg);
     }
 
+    #[tracing::instrument(name = "websocket_incoming", skip_all, fields(addr = %addr))]
+    async fn process_incoming(
+        addr: SocketAddr,
+        client_messages_sender: mpsc::Sender<ClientMessage>,
+        mut ws_receiver: SplitStream<WebSocket>,
+    ) {
+        while let Some(Ok(msg)) = ws_receiver.next().await {
+            tracing::trace!(?msg, "Message received");
+
+            let msg = match msg {
+                Message::Text(text) => text,
+                Message::Binary(_) => {
+                    tracing::warn!("Binary message received. Ignoring");
+                    continue;
+                }
+                Message::Ping(_) => {
+                    tracing::warn!("Ping message received. Ignoring");
+                    continue;
+                }
+                Message::Pong(_) => {
+                    tracing::warn!("Pong message received. Ignoring");
+                    continue;
+                }
+                Message::Close(_) => {
+                    tracing::warn!("Close message received. Ignoring");
+                    continue;
+                }
+            };
+
+            let msg = match serde_json::from_str::<ClientMessage>(&msg) {
+                Ok(msg) => msg,
+                Err(err) => {
+                    tracing::warn!(?err, "Failed to parse message");
+                    continue;
+                }
+            };
+
+            match client_messages_sender.send(msg).await {
+                Ok(_) => {}
+                Err(err) => {
+                    tracing::error!(?err, "Failed to forward message to state");
+                    break;
+                }
+            }
+        }
+
+        tracing::debug!("Receiver closed");
+    }
+
+    // FIXME: Closes only after close when we attempt to send a message and it fails
+    #[tracing::instrument(name = "websocket_outgoing", skip_all)]
+    async fn process_outgoing(
+        mut broadcast_receiver: broadcast::Receiver<ServerMessage>,
+        mut ws_sender: SplitSink<WebSocket, Message>,
+    ) {
+        while let Ok(msg) = broadcast_receiver.recv().await {
+            let msg = serde_json::to_string(&msg).unwrap_or_default();
+            let msg = Message::Text(msg);
+
+            tracing::trace!(?msg, "Sending message");
+
+            match ws_sender.send(msg).await {
+                Ok(_) => {}
+                Err(err) => {
+                    tracing::error!(?err, "Failed to send message");
+                    break;
+                }
+            }
+        }
+
+        tracing::debug!("Sender closed");
+    }
+
     #[tracing::instrument(name = "websocket", skip_all, fields(addr = %addr))]
     pub async fn accept_connection(
         &self,
@@ -31,78 +106,31 @@ impl ConnectionManager {
     ) {
         tracing::info!(%user_agent, "Connected");
 
-        let (mut sender, mut receiver) = socket.split();
-        let mut broadcast_receiver = self.broadcast_sender.subscribe();
+        let (ws_sender, ws_receiver) = socket.split();
+        let broadcast_receiver = self.broadcast_sender.subscribe();
 
-        let mut recv_task = tokio::spawn(async move {
-            while let Some(Ok(msg)) = receiver.next().await {
-                tracing::trace!(?msg, ?addr, "Websocket message received");
+        let mut recv_task = tokio::spawn(ConnectionManager::process_incoming(
+            addr,
+            client_messages_sender,
+            ws_receiver,
+        ));
 
-                let msg = match msg {
-                    Message::Text(text) => text,
-                    Message::Binary(_) => {
-                        tracing::warn!("Binary message received. Ignoring");
-                        continue;
-                    }
-                    Message::Ping(_) => {
-                        tracing::warn!("Ping message received. Ignoring");
-                        continue;
-                    }
-                    Message::Pong(_) => {
-                        tracing::warn!("Pong message received. Ignoring");
-                        continue;
-                    }
-                    Message::Close(_) => {
-                        tracing::warn!("Close message received. Ignoring");
-                        continue;
-                    }
-                };
-
-                let msg = match serde_json::from_str::<ClientMessage>(&msg) {
-                    Ok(msg) => msg,
-                    Err(err) => {
-                        tracing::warn!(?err, ?addr, "Failed to parse websocket message");
-                        continue;
-                    }
-                };
-
-                match client_messages_sender.send(msg).await {
-                    Ok(_) => {}
-                    Err(err) => {
-                        tracing::error!(?err, ?addr, "Failed forward websocket message to state");
-                        break;
-                    }
-                }
-            }
-
-            tracing::debug!(?addr, "Websocket closed");
-        });
-
-        let mut send_task = tokio::spawn(async move {
-            while let Ok(msg) = broadcast_receiver.recv().await {
-                let msg = serde_json::to_string(&msg).unwrap_or_default();
-                let msg = Message::Text(msg);
-
-                tracing::trace!(?msg, ?addr, "Sending websocket message");
-
-                match sender.send(msg).await {
-                    Ok(_) => {}
-                    Err(err) => {
-                        tracing::error!(?err, ?addr, "Failed to send websocket message");
-                        break;
-                    }
-                }
-            }
-        });
+        let mut send_task = tokio::spawn(ConnectionManager::process_outgoing(
+            broadcast_receiver,
+            ws_sender,
+        ));
 
         tokio::select! {
             _ = (&mut send_task)  => {
-                recv_task.abort();
+                //recv_task.abort();
             },
             _ = (&mut recv_task) => {
-                send_task.abort();
+                //send_task.abort();
             }
         }
+
+        // recv_task.await.unwrap_or_default();
+        // send_task.await.unwrap_or_default();
 
         tracing::debug!("Context destroyed");
     }
