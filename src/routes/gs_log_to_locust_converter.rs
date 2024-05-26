04e40b44a802b450a1baa85cda9145ea0243e8b7
Juri Tomak <juri.tomak@gmail.com>
2024-05-26 16:55:54 +0000

    fix(GSLogToLocustConverter): typo

--- src/routes/gs_log_to_locust_converter.rs
+++ src/routes/gs_log_to_locust_converter.rs
@@ -12,35 +12,35 @@ use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 #[derive(Serialize, ToSchema)]
-pub struct GsLogToLocstConverterOkResponse {
+pub struct GsLogToLocustConverterOkResponse {
     /// Task id that was scheduled for running
     #[schema(example = "0")]
     id: String,
 }
 
 #[derive(Serialize, ToSchema)]
-pub enum GsLogToLocstConverterErrorResponse {
+pub enum GsLogToLocustConverterErrorResponse {
     NotFound,
 }
 
-impl From<GsLogToLocstConverterError> for GsLogToLocstConverterErrorResponse {
+impl From<GsLogToLocstConverterError> for GsLogToLocustConverterErrorResponse {
     fn from(err: GsLogToLocstConverterError) -> Self {
         match err {
-            GsLogToLocstConverterError::NotFound => GsLogToLocstConverterErrorResponse::NotFound,
+            GsLogToLocstConverterError::NotFound => GsLogToLocustConverterErrorResponse::NotFound,
         }
     }
 }
 
-impl IntoResponse for GsLogToLocstConverterOkResponse {
+impl IntoResponse for GsLogToLocustConverterOkResponse {
     fn into_response(self) -> Response {
         (StatusCode::CREATED, Json(self)).into_response()
     }
 }
 
-impl IntoResponse for GsLogToLocstConverterErrorResponse {
+impl IntoResponse for GsLogToLocustConverterErrorResponse {
     fn into_response(self) -> Response {
         match self {
-            GsLogToLocstConverterErrorResponse::NotFound => {
+            GsLogToLocustConverterErrorResponse::NotFound => {
                 (StatusCode::NOT_FOUND, Json(self)).into_response()
             }
         }
@@ -48,7 +48,7 @@ impl IntoResponse for GsLogToLocstConverterErrorResponse {
 }
 
 #[derive(Deserialize)]
-pub struct GsLogToLocstConverterQuery {
+pub struct GsLogToLocustConverterQuery {
     /// Name of the project
     project_name: String,
 }
@@ -56,14 +56,16 @@ pub struct GsLogToLocstConverterQuery {
 /// TODO: Add description
 #[utoipa::path(
     post,
-    path = "/api/gs_log_to_locst_converter", 
+    path = "/api/gs_log_to_locust_converter", 
     params(
         ("chat_id" = String, Query, description = "Chat id. generated using the `/api/request_chat_id` endpoint"),
         ("project_name" = String, Query, description = "Name of the project.")
     ),
     tag = "convert",
     responses(
-        (status = 201, description = "Task was scheduled for running", body = GsLogToLocstConverterOkResponse, example = json!(GsLogToLocstConverterOkResponse{id: String::from("some-id")})),
+        (status = 201, description = "Task was scheduled for running", body = GsLogToLocu
+    stConverterOkResponse, example = json!(GsLogToLocu
+        stConverterOkResponse{id: String::from("some-id")})),
         (status = 400, description = "Chat id missing, Api key missing"),
         (status = 401, description = "Api key invalid"),
     ),
@@ -71,16 +73,16 @@ pub struct GsLogToLocstConverterQuery {
         ("api_key" = []),
     ),
 )]
-pub async fn gs_log_to_locst_converter(
+pub async fn gs_log_to_locust_converter(
     State(state): State<ApiState>,
     ChatId(chat_id): ChatId,
-    Query(query): Query<GsLogToLocstConverterQuery>,
-) -> Result<GsLogToLocstConverterOkResponse, GsLogToLocstConverterErrorResponse> {
+    Query(query): Query<GsLogToLocustConverterQuery>,
+) -> Result<GsLogToLocustConverterOkResponse, GsLogToLocustConverterErrorResponse> {
     let project_name = query.project_name;
 
     let id = state
-        .run_gs_log_to_locst_converter_task(chat_id, project_name)
+        .run_gs_log_to_locust_converter_task(chat_id, project_name)
         .await?;
 
-    Ok(GsLogToLocstConverterOkResponse { id })
+    Ok(GsLogToLocustConverterOkResponse { id })
 }
